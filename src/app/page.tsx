1bfc7354ece39c910bdf1ba7b393fac03cdf94a2
mhdali <mhdaliorabi@gmail.com>
2026-05-25 15:15:39 +0100

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,37 +10,16 @@ import InsightsSection from "@/components/insights-section";
 import VisualizationSection from "@/components/visualization-section";
 import FooterSection from "@/components/footer-section";
 
-export default async function Home() {
-  const medicineName = "Tylenol";
-
-  // FDA DATA
-  const fdaData = await getMedicineData(medicineName);
-
-  // AI DATA
-  const aiData = await cloudAI(medicineName);
-
+export default function Home() {
   return (
     <main className="min-h-screen bg-background">
-      <MedicineScanner />
       <HeroSection />
 
+      <MedicineScanner />
+
       <FeaturesSection />
-
-      <AiAnalysisCard
-        aiData={aiData}
-        fdaData={fdaData}
-      />
-
-      {/* AI + FDA DATA */}
-      <AnalysisSection
-        aiData={aiData}
-        fdaData={fdaData}
-      />
-
       <InsightsSection />
-
       <VisualizationSection />
-
       <FooterSection />
     </main>
   );
